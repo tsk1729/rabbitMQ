58c6eb144d68815a027a5b480ab66c4efab17b99
Sandeep <talapally.kumar@oracle.com>
2025-01-10 00:35:37 +0530

    added template links

--- instagram_utils.py
+++ instagram_utils.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 
 from app_logger import logger
 from config import SEND_MESSAGE, REPLY_TO_COMMENT, PRIVATE_REPLY_TO_COMMENT
+from urllib.parse import urlparse
 
 
 async def send_message(sender_id: str, recipient_id: str, text: str, access_token: str):
@@ -48,6 +49,50 @@ async def reply_to_comment(comment_id: str, message: str, access_token: str):
         raise HTTPException(status_code=400, detail=str(e))
 
 
+async def  is_url(text):
+    parsed_url = urlparse(text)
+    logger.info("Parsed url:",parsed_url)
+    logger.info("Result:",bool(parsed_url.netloc))
+    return bool(parsed_url.netloc)
+
+
+async def generate_payload(text, comment_id):
+    flag = await is_url(text)
+    if flag:
+        payload = {
+            "recipient": {
+                "comment_id": comment_id
+            },
+            "message": {
+                "attachment":
+                    {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "generic",
+                            "elements": [
+                                {
+                                    "title": "Check out this product!",
+                                    "subtitle": "Click the button below to view more details.",
+                                    "image_url": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDFqZG1wZnd1ZjBndnNzd3MwNnY1NjdhOXdpbWdpeHp1cWx2OG4yMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/g80laQY3eau4DRrdjF/giphy.gif",
+                                    "buttons": [
+                                        {
+                                            "type": "web_url",
+                                            "url": text,
+                                            "title": "Product link"
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    }}}
+    else:
+        payload = {
+            "recipient": {"comment_id": comment_id},
+            "message": {"text": text}
+        }
+    return payload
+
+
 async def private_message_to_comment(comment_id,message,profile_id,token):
     logger.info("Inside private_message_to_comment function")
     url = PRIVATE_REPLY_TO_COMMENT.replace("{user_id}",profile_id)
@@ -56,14 +101,7 @@ async def private_message_to_comment(comment_id,message,profile_id,token):
         "Content-Type": "application/json",
         "Authorization": f"Bearer {token}"
     }
-    payload = {
-        "recipient": {
-            "comment_id": comment_id
-        },
-        "message": {
-            "text": message
-        }
-    }
+    payload = generate_payload(message,comment_id)
     logger.info("Calling send API with headers: {} payload:{} ".format(headers, payload))
     try:
         response = requests.post(url, json=payload, headers=headers)
