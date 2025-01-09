43c3d9447e97e9aa50b9a9a6758f90b3b51a0288
Sandeep <talapally.kumar@oracle.com>
2025-01-10 01:18:51 +0530

    added

--- instagram_utils.py
+++ instagram_utils.py
@@ -17,14 +17,15 @@ async def send_message(sender_id: str, recipient_id: str, text: str, access_toke
         "Content-Type": "application/json"
     }
     try:
-        logger.info("Calling send API with headers: {} payload:{} ".format(headers,payload))
+        logger.info("Calling send API with headers: {} payload:{} ".format(headers, payload))
         response = requests.post(SEND_MESSAGE.format(sender_id=sender_id), json=payload, headers=headers)
         response.raise_for_status()
         logger.info(f"Response received is {response.json()}")
         logger.info(f"Message sent to {recipient_id} from {sender_id} successfully")
         logger.info("Exiting send_message function")
     except requests.exceptions.RequestException as e:
-        logger.error(f"Unable to send message from recipient {recipient_id} to sender {sender_id} with error message: {e}")
+        logger.error(
+            f"Unable to send message from recipient {recipient_id} to sender {sender_id} with error message: {e}")
         logger.info("Exiting send_message function")
         raise HTTPException(status_code=400, detail=str(e))
 
@@ -49,43 +50,44 @@ async def reply_to_comment(comment_id: str, message: str, access_token: str):
         raise HTTPException(status_code=400, detail=str(e))
 
 
-def  is_url(text):
+def is_url(text):
     parsed_url = urlparse(text)
-    logger.info("Parsed url:",parsed_url)
-    logger.info("Result:",bool(parsed_url.netloc))
+    logger.info("Parsed url:", parsed_url)
+    logger.info("Result:", bool(parsed_url.netloc))
     return bool(parsed_url.netloc)
 
 
 def generate_payload(text, comment_id):
-    logger.info("Inside Genrate Payload")
-    flag =  is_url(text)
+    logger.info("Inside Generate Payload")
+    flag = is_url(text)
     if flag:
         payload = {
             "recipient": {
-                "comment_id": comment_id
+                "id": comment_id
             },
             "message": {
-                "attachment":
-                    {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "generic",
-                            "elements": [
-                                {
-                                    "title": "Check out this product!",
-                                    "subtitle": "Click the button below to view more details.",
-                                    "image_url": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDFqZG1wZnd1ZjBndnNzd3MwNnY1NjdhOXdpbWdpeHp1cWx2OG4yMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/g80laQY3eau4DRrdjF/giphy.gif",
-                                    "buttons": [
-                                        {
-                                            "type": "web_url",
-                                            "url": text,
-                                            "title": "Product link"
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    }}}
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": "Check out this product!",
+                                "subtitle": "Click the button below to view more details.",
+                                "image_url": "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMDFqZG1wZnd1ZjBndnNzd3MwNnY1NjdhOXdpbWdpeHp1cWx2OG4yMSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/g80laQY3eau4DRrdjF/giphy.gif",
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url":text,
+                                        "title": "Product link"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        }
     else:
         payload = {
             "recipient": {"comment_id": comment_id},
@@ -95,15 +97,15 @@ def generate_payload(text, comment_id):
     return payload
 
 
-async def private_message_to_comment(comment_id,message,profile_id,token):
+async def private_message_to_comment(comment_id, message, profile_id, token):
     logger.info("Inside private_message_to_comment function")
-    url = PRIVATE_REPLY_TO_COMMENT.replace("{user_id}",profile_id)
+    url = PRIVATE_REPLY_TO_COMMENT.replace("{user_id}", profile_id)
     logger.info("url {url}")
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {token}"
     }
-    payload =  generate_payload(message,comment_id)
+    payload = generate_payload(message, comment_id)
     logger.info("Calling send API with headers: {} payload:{} ".format(headers, payload))
     try:
         response = requests.post(url, json=payload, headers=headers)
@@ -114,4 +116,3 @@ async def private_message_to_comment(comment_id,message,profile_id,token):
         logger.error(f"Unable to send private message for comment_id {comment_id} with error message: {e}")
         logger.info("Exiting private_message_to_comment function")
         raise HTTPException(status_code=500, detail=str(e))
-
